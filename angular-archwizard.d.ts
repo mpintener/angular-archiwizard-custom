3a7ece1ff4adb1b3cabada088bebe07220b973dc
Martina Pintener <mpintener@gmail.com>
2018-10-05 16:33:21 -0300

    initial commit

---
+++ angular-archwizard.d.ts
@@ -0,0 +1,22 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './index';
+export { WizardCompletionStepComponent as ɵh } from './lib/components/wizard-completion-step.component';
+export { WizardNavigationBarComponent as ɵg } from './lib/components/wizard-navigation-bar.component';
+export { WizardStepComponent as ɵf } from './lib/components/wizard-step.component';
+export { WizardComponent as ɵa } from './lib/components/wizard.component';
+export { EnableBackLinksDirective as ɵn } from './lib/directives/enable-back-links.directive';
+export { GoToStepDirective as ɵj } from './lib/directives/go-to-step.directive';
+export { NextStepDirective as ɵk } from './lib/directives/next-step.directive';
+export { OptionalStepDirective as ɵm } from './lib/directives/optional-step.directive';
+export { PreviousStepDirective as ɵl } from './lib/directives/previous-step.directive';
+export { ResetWizardDirective as ɵr } from './lib/directives/reset-wizard.directive';
+export { SelectedStepDirective as ɵq } from './lib/directives/selected-step.directive';
+export { WizardCompletionStepDirective as ɵp } from './lib/directives/wizard-completion-step.directive';
+export { WizardStepSymbolDirective as ɵe } from './lib/directives/wizard-step-symbol.directive';
+export { WizardStepTitleDirective as ɵd } from './lib/directives/wizard-step-title.directive';
+export { WizardStepDirective as ɵo } from './lib/directives/wizard-step.directive';
+export { WizardState as ɵb } from './lib/navigation/wizard-state.model';
+export { WizardCompletionStep as ɵi } from './lib/util/wizard-completion-step.interface';
+export { WizardStep as ɵc } from './lib/util/wizard-step.interface';
---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export * from './lib/components';
+export * from './lib/directives';
+export * from './lib/navigation';
+export * from './lib/util';
+export { ArchwizardModule } from './lib/archwizard.module';
